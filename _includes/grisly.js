393e33ec11c096669732e23d23ef6a0232762236
Ben Styles <enquiries@leeds-ebooks.co.uk>
2015-04-03 21:55:57 +0100

    Removed animated header, created animated footer

--- _includes/grisly.js
+++ _includes/grisly.js
@@ -2,14 +2,15 @@ window.Q=function(fn) {setTimeout(fn,0);};
 window.log=function(x) {console.log(x);};
 
 function init() {
-  var txtAdj=localStorage.getItem("txtAdj"),
-      firstSpan=document.getElementById("firstGrisly"),
-      prevSpan=firstSpan,
-      mainSpan=document.getElementById("grisly"),
-      grislies=["Horrid","Slimy","Weird","Dreadful","Grim","Kooky","Scandalous","Wild","Nasty","Lousy","Grisly"],
-      spans=[],
-      count=0,
-      newSpan,
+  var
+      // txtAdj=localStorage.getItem("txtAdj"),
+      // firstSpan=document.getElementById("firstGrisly"),
+      // prevSpan=firstSpan,
+      // mainSpan=document.getElementById("grisly"),
+      // grislies=["Horrid","Slimy","Weird","Dreadful","Grim","Kooky","Scandalous","Wild","Nasty","Lousy","Grisly"],
+      // spans=[],
+      // count=0,
+      // newSpan,
       whichTransitionEvent=function whichTransitionEvent() {
         var t,
             el=document.createElement('fakeelement'),
@@ -25,34 +26,61 @@ function init() {
           }
         }
       },
-      transitionEvent=whichTransitionEvent()||"transitionend",
-      loadedGrisly,
-      stickyElements=document.getElementsByClassName('sticky'),
-      images=document.querySelectorAll('img[src^="/images"], img[src*="_posts"]');
+      transitionEvent = whichTransitionEvent() || "transitionend",
+      // loadedGrisly,
+      stickyElements = document.getElementsByClassName('sticky'),
+      images = document.querySelectorAll('img[src^="/images"], img[src*="_posts"]'),
+      sitedescSep = '#',
+      sitedescInt = 8000,
+      sitedesc = document.getElementById('sitedesc'),
+      sitedescArr = sitedesc.textContent.split(sitedescSep),
+      sitedescCounter = 0;
 
-  function picker(event) {
-    function handler(e) {
-      prevSpan.removeEventListener(transitionEvent, handler, false);
-      prevSpan.parentNode.removeChild(prevSpan);
-      localStorage.setItem("txtAdj", grislies[count]);
-      if (count===grislies.length-1) {count=0;} else {count+=1;}
-      Q(function() {
-        prevSpan.className="grislies";
-        prevSpan=newSpan;
-      });
+  // function picker(event) {
+  //   function handler(e) {
+  //     prevSpan.removeEventListener(transitionEvent, handler, false);
+  //     prevSpan.parentNode.removeChild(prevSpan);
+  //     localStorage.setItem("txtAdj", grislies[count]);
+  //     if (count===grislies.length-1) {count=0;} else {count+=1;}
+  //     Q(function() {
+  //       prevSpan.className="grislies";
+  //       prevSpan=newSpan;
+  //     });
+  //   }
+  //   if (MOH.isHome) {
+  //     event.preventDefault();
+  //     newSpan=spans[count];
+  //     mainSpan.appendChild(newSpan);
+  //     prevSpan.className+=" posAbs";
+  //     prevSpan.addEventListener(transitionEvent, handler, false);
+  //     Q(function() {
+  //       newSpan.className+=" grisly-slide-in";
+  //       prevSpan.className+=" grisly-slide-out";
+  //     });
+  //   }
+  // }
+
+
+  function changeSitedesc(newText) {
+    function handler() {
+      sitedesc.removeEventListener(transitionEvent, handler);
+      sitedesc.textContent = newText;
+      setTimeout(function() {
+        sitedesc.style.opacity = 1;
+      }, 0);
     }
-    if (MOH.isHome) {
-      event.preventDefault();
-      newSpan=spans[count];
-      mainSpan.appendChild(newSpan);
-      prevSpan.className+=" posAbs";
-      prevSpan.addEventListener(transitionEvent, handler, false);
-      Q(function() {
-        newSpan.className+=" grisly-slide-in";
-        prevSpan.className+=" grisly-slide-out";
-      });
-    }
+    sitedesc.addEventListener(transitionEvent, handler);
+    sitedesc.style.opacity = 0;
   }
+
+  sitedesc.textContent = sitedescArr[sitedescCounter];
+
+  setInterval(function() {
+    sitedescCounter = sitedescCounter < (sitedescArr.length - 1) ?
+      sitedescCounter + 1 :
+      0;
+    changeSitedesc(sitedescArr[sitedescCounter]);
+  }, sitedescInt);
 
   for (var i = 0; i < images.length; i++) {
     images[i].src= ~images[i].src.indexOf('_posts') ?
@@ -60,26 +88,26 @@ function init() {
       "{{site.baseurl}}"+(images[i].src.replace('http://leeds-ebooks.github.io',''));
   }
 
-  if (txtAdj) {firstSpan.innerHTML=txtAdj;}
+  // if (txtAdj) {firstSpan.innerHTML=txtAdj;}
 
-  for (var i = 0; i < grislies.length; i++) {
-    var span=document.createElement("span");
-    span.innerHTML=grislies[i];
-    span.className="grislies";
-    spans.push(span);
-  }
+  // for (var i = 0; i < grislies.length; i++) {
+  //   var span=document.createElement("span");
+  //   span.innerHTML=grislies[i];
+  //   span.className="grislies";
+  //   spans.push(span);
+  // }
 
-  loadedGrisly=grislies.indexOf(firstSpan.innerHTML);
-  count=loadedGrisly===grislies.length-1 ?
-    0 :
-    loadedGrisly+1;
+  // loadedGrisly=grislies.indexOf(firstSpan.innerHTML);
+  // count=loadedGrisly===grislies.length-1 ?
+  //   0 :
+  //   loadedGrisly+1;
 
   for (var j=stickyElements.length-1;j>=0;j--) {
     Stickyfill.add(stickyElements[j]);
   }
 
   // document.querySelector(".site-title").addEventListener("tap", picker, false);
-  document.querySelector(".site-title").addEventListener("click", picker, false);
+  // document.querySelector(".site-title").addEventListener("click", picker, false);
   document.removeEventListener("DOMContentLoaded", init, false);
 }
 document.addEventListener('DOMContentLoaded', init, false);
