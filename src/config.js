2bb7860aa960275ede3a3ee1b1a3c8398e91a950
Ben Styles <enquiries@leeds-ebooks.co.uk>
2016-05-07 13:32:01 +0100

    fixed NaN year input bug, added local browser-sync

--- src/config.js
+++ src/config.js
@@ -1,21 +1,22 @@
 import rivets from 'rivets'
 
 Object.assign(rivets.formatters, {
-  matchesRange: (range, begin, end) => {
+  matchesRange(range, begin, end) {
     const isRange = range.includes('-'),
-    min = parseInt(isRange ? range.split('-')[0] : range, 10),
-    max = parseInt(isRange ? range.split('-')[1] : range, 10),
-    b = parseInt(begin, 10),
-    e = parseInt(end, 10)
+          min = parseInt(isRange ? range.split('-')[0] : range, 10),
+          max = parseInt(isRange ? range.split('-')[1] : range, 10),
+          b = parseInt(begin, 10),
+          e = parseInt(end, 10);
 
-    if (b <= min && max <= e) return true
-    else return false
+    return b <= min && max <= e
   },
 
   toBC: {
-    read: val => {
-      const int = parseInt(val, 10)
-      return int < 0 ? `${Math.abs(int)} BC` : `${val} AD`
+    read(val) {
+      const int = Number.parseInt(val, 10)
+      return int < 0 ?
+        `${Math.abs(int)} BC` :
+        `${Number.isNaN(int) ? 0 : val} AD`
     },
     publish: val => val.includes('BC') ? `${0 - parseInt(val, 10)}` : `${parseInt(val, 10)}`
   }
